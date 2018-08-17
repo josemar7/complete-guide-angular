38d0dda982bd5977ef8d6d1a99ad3574da35a5b6
josemar7 <josemar7@gmail.com>
2018-08-17 09:50:00 +0200

    mi commit!

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import 'rxjs/Rx';
 
 @Injectable()
 export class DataStorageService {
@@ -16,9 +17,19 @@ export class DataStorageService {
 
   getRecipes() {
     this.http.get('https://ng-recipe-book-d0fc6.firebaseio.com/recipes.json')
-    .subscribe(
+    .map(
       (response: Response) => {
         const recipes: Recipe[] = response.json();
+        for (const recipe of recipes) {
+          if (!recipe['ingredients']) {
+            recipe['ingredients'] = [];
+          }
+        }
+        return recipes;
+      }
+    )
+    .subscribe(
+      (recipes: Recipe[]) => {
         this.recipeService.setRecipes(recipes);
       }
     );
