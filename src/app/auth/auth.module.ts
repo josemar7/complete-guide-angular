790f0235d7725892df6dcf068d0e96cb19fda230
josemar <josemar7@gmail.com>
2018-08-20 08:37:57 +0200

    mi commit!

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [
+    SigninComponent,
+    SignupComponent
+  ],
+  imports: [
+    FormsModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule {
+
+}
